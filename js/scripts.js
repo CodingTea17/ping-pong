8e4ea065cda22f29ed5c35669f004a570b4e87f2
Dawson M <dawson.mortenson@gmail.com>
2017-08-18 12:37:31 -0700

    add branching to determine if it should write the number, ping, pong, or pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // Backend Code
 var pingPong = function(number){
   for(var i = 1; i <= number; i++){
-    $("ul#numbers").append("<li>" + i + "</li>");
+    if (i % 15 === 0) {
+      $("ul#numbers").append("<li>" + "ping-pong" + "</li>");
+    } else if (i % 5 === 0) {
+      $("ul#numbers").append("<li>" + "pong" + "</li>");
+    } else if (i % 3 === 0) {
+      $("ul#numbers").append("<li>" + "ping" + "</li>");
+    } else {
+      $("ul#numbers").append("<li>" + i + "</li>");
+    }
   }
 }
 
