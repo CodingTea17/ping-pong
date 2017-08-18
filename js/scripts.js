67c707d9401637d70a5df15c9e2327170d80ed4d
Dawson M <dawson.mortenson@gmail.com>
2017-08-18 16:42:23 -0700

    add error checking to the user's input

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 // Backend Code
 var pingPong = function(number){
-  for(var i = 1; i <= number; i++){
-    if (i % 15 === 0) {
-      $("ul#numbers").append("<li>" + "ping-pong" + "</li>");
-    } else if (i % 5 === 0) {
-      $("ul#numbers").append("<li>" + "pong" + "</li>");
-    } else if (i % 3 === 0) {
-      $("ul#numbers").append("<li>" + "ping" + "</li>");
-    } else {
-      $("ul#numbers").append("<li>" + i + "</li>");
+  // isNan checks to see if the user's input is a number
+  if (isNaN(number)){
+    alert("Please enter a number");
+    return false;
+  } else {
+    for(var i = 1; i <= number; i++){
+      if (i % 15 === 0) {
+        $("ul#numbers").append("<li>" + "ping-pong" + "</li>");
+      } else if (i % 5 === 0) {
+        $("ul#numbers").append("<li>" + "pong" + "</li>");
+      } else if (i % 3 === 0) {
+        $("ul#numbers").append("<li>" + "ping" + "</li>");
+      } else {
+        $("ul#numbers").append("<li>" + i + "</li>");
+      }
     }
+    return true;
   }
 }
 
@@ -19,7 +26,8 @@ $(document).ready(function(){
     event.preventDefault();
     var inputNum = $("#user-input").val();
     $("#numbers").empty();
-    $("#results").show();
-    pingPong(inputNum);
+    if (pingPong(inputNum)){
+      $("#results").show();
+    };
   });
 });
