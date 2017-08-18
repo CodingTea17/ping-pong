d77027badf1a5123e956a09cd18479a8fbf8b599
Dawson M <dawson.mortenson@gmail.com>
2017-08-18 12:24:15 -0700

    add a function that will count up to a given number and append <li>s with each number

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
+// Backend Code
+var pingPong = function(number){
+  for(var i = 1; i < number; i++){
+    $("ul#numbers").append("<li>" + i + "</li>");
+  }
+}
+
+// Frontend Code
 $(document).ready(function(){
   $("form#number").submit(function(event){
     event.preventDefault();
     var inputNum = $("#user-input").val();
-    alert(inputNum);
   });
 });
